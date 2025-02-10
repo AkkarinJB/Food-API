992b35344af1f0ce1c46178b4a6cbb818bc45da6
AkkarinJB <akkarinjujaiboon11087@gmail.com>
2025-02-10 22:52:06 +0700

    update

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import joblib
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
@@ -5,16 +6,15 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest, f_regression
 
 def train_knn():
-    # โหลดข้อมูล
     food_df = pd.read_csv("data/pred_food.csv")
 
-    # ทำ Data Cleaning
+    #Data Cleaning
     columns_to_fill = ["Glycemic Index", "Calories", "Carbohydrates", "Protein", "Fat", "Fiber Content"]
     for col in columns_to_fill:
         median_value = food_df[col].median()
         food_df[col] = food_df[col].replace(0, median_value)
 
-    # Scaling ข้อมูล
+    # Scaling Data
     scaler = MinMaxScaler()
     food_df[columns_to_fill] = scaler.fit_transform(food_df[columns_to_fill])
 
@@ -29,10 +29,14 @@ def train_knn():
     knn_model = KNeighborsRegressor(n_neighbors=20, weights='distance')
     knn_model.fit(X_selected, y_food)
 
+    joblib.dump(knn_model, "knn_model.pkl")
+
     return knn_model, food_df, selected_features
 
-def recommend_food(knn_model, food_df, selected_features, user_input):
-    user_array = np.array([[user_input.calories, user_input.carbohydrates, user_input.protein]])
+# train_knn()
+
+def recommend_food(knn_model, food_df, selected_features, user_input, daily_calories):
+    user_array = np.array([[daily_calories, user_input.carbohydrates, user_input.protein]])
     pred_gi = knn_model.predict(user_array)[0]
 
     food_df["Predicted GI Diff"] = abs(food_df["Glycemic Index"] - pred_gi)
